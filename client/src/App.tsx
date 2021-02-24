b7eb80fa5779d83ed83d8968e377b31a45e04898
Fanghan Hu <Fanghan.Hu@gmail.com>
2021-02-23 19:55:33 -0600

    put test page stuf into its own file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,13 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Button from "./components/Button";
-import Panel from "./components/Panel";
-import PanelBody from "./components/PanelBody";
-import { Color } from "./util/Color";
-import LabelBar from "./components/LabelBar";
-import PanelHeader from "./components/PanelHeader";
-import InputGroup from "./components/InputGroup";
-import Label from "./components/Label";
-import TextInput from "./components/TextInput";
+import Test from "./pages/test";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/" exact>
-          <Panel>
-            <PanelHeader>The Header</PanelHeader>
-            <PanelBody>
-              <InputGroup>
-                <Label htmlFor="#input1">some name</Label>
-                <TextInput placeholder="some text" id="input1"/>
-              </InputGroup>
-              <Button themeColor={Color.kiwi_green} style={{fontSize:"2rem"}}>Button</Button>
-            </PanelBody>
-            <LabelBar>The Label Bar</LabelBar>
-          </Panel>
-        </Route>
+        <Route path="/" exact component={Test}/>
       </Router>
     </div>
   );
---
+++ client/src/pages/test.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Button from "../components/Button";
+import InputGroup from "../components/InputGroup";
+import Label from "../components/Label";
+import LabelBar from "../components/LabelBar";
+import Panel from "../components/Panel";
+import PanelBody from "../components/PanelBody";
+import PanelHeader from "../components/PanelHeader";
+import TextInput from "../components/TextInput";
+import { Color } from "../util/Color";
+
+export default function Test(): JSX.Element {
+  return (
+    <div>
+      <Panel>
+        <PanelHeader>The Header</PanelHeader>
+        <PanelBody>
+          <InputGroup>
+            <Label htmlFor="#input1">some name</Label>
+            <TextInput placeholder="some text" id="input1" />
+          </InputGroup>
+          <Button themeColor={Color.kiwi_green} style={{ fontSize: "2rem" }}>
+            Button
+          </Button>
+        </PanelBody>
+        <LabelBar>The Label Bar</LabelBar>
+      </Panel>
+    </div>
+  );
+}
