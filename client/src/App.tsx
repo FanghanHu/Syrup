5f6105ad99371b55822f6b300f1d20c54d8589e4
Fanghan Hu <Fanghan.Hu@gmail.com>
2021-02-24 13:33:04 -0600

    added side menu for backoffice

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Test from "./pages/test";
+import Setup from "./pages/setup";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Route path="/" exact component={Test}/>
+        <Route path="/setup" exact component={Setup}/>
       </Router>
     </div>
   );
---
+++ client/src/pages/setup.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Button from "../components/Button";
+import ExpandableSideMenu from "../components/ExpandableSideMenu";
+import LabelBar from "../components/LabelBar";
+import { Color } from "../util/Color";
+
+export default function Setup(): JSX.Element {
+
+    const ButtonTheme = Color.kiwi_green;
+
+    return (
+        <Container fluid>
+            <ExpandableSideMenu>
+                
+                <div className="d-flex flex-column justfy-content-center p-2 h-100">
+                    <div className="d-flex flex-column flex-grow-1">
+                        <LabelBar themeColor={Color.sky_blue} className="m-2 h3 text-center text-nowrap p-3">
+                            <i className="fas fa-cog"></i> Back Office
+                        </LabelBar>
+                        <Button themeColor={ButtonTheme} className="m-1">
+                            User Setup
+                        </Button>
+                        <Button themeColor={ButtonTheme} className="m-1">
+                            Menu Setup
+                        </Button>
+                        <Button themeColor={ButtonTheme} className="m-1">
+                            Table Setup
+                        </Button>
+                    </div>
+                    <Button themeColor={Color.fire_red} className="m-1">
+                        Exit
+                    </Button>
+                </div>
+            </ExpandableSideMenu>
+        </Container>
+    );
+}
