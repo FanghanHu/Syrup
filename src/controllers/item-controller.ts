5a246c2d70fa964fe8f7b528022d46a3a2db97e1
Fanghan Hu <Fanghan.Hu@gmail.com>
2021-03-06 20:50:31 -0600

    finish item route

--- src/controllers/item-controller.ts
+++ src/controllers/item-controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import db from "../models";
 import { catchError, isIdValid } from "../utils/helpers";
+import { isTokeninvalid } from "../utils/login-token";
 
 
 
@@ -22,12 +23,20 @@ export const getItem = catchError(async (req:Request, res:Response) => {
 });
 
 export const createItem = catchError(async (req:Request, res:Response) => {
+    //check client token
+    if(isTokeninvalid(req, res)) return;
+    const ServerId = req.body.userId;
+    
     const itemData = req.body;
     const item = await db.Item.create(itemData);
     return res.status(200).json(item);
 });
 
 export const deleteItem = catchError(async (req:Request, res:Response) => {
+    //check client token
+    if(isTokeninvalid(req, res)) return;
+    const ServerId = req.body.userId;
+
     if(!isIdValid(req, res)) return;
     const id = req.body.id;
 
@@ -36,8 +45,12 @@ export const deleteItem = catchError(async (req:Request, res:Response) => {
 });
 
 export const updateItem = catchError(async (req: Request, res: Response) => {
+    //check client token
+    if(isTokeninvalid(req, res)) return;
+    const ServerId = req.body.userId;
+
     if(!isIdValid(req, res)) return;
-    const id = req.body.id;
-    await db.Item.destroy({where: {id}});
+    const item = req.body;
+    await db.Item.update({...item, id: undefined},{where: {id: item.id}});
     return res.status(200).send("Done.");
 });
--- src/routes/api/item.ts
+++ src/routes/api/item.ts
@@ -1,6 +1,10 @@
-import { listItems } from "../../controllers/item-controller";
+import { createItem, deleteItem, getItem, listItems, updateItem } from "../../controllers/item-controller";
 
 const router = require("express").Router();
 router.get('/list', listItems);
+router.get('/get', getItem);
+router.post('/create', createItem);
+router.post('/delete', deleteItem);
+router.post('/update', updateItem);
 
 module.exports = router;
