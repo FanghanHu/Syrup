4e028cfe97b7603b4e238e632249f16759f72acc
Fanghan Hu <Fanghan.Hu@gmail.com>
2021-03-06 18:56:57 -0600

    handle uncaught errors

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -1,6 +1,19 @@
 const router = require("express").Router();
+const env = process.env.NODE_ENV || 'development';
 
 router.use("/login", require("./login"));
-router.use("/order", require('./order'));
+router.use("/order", require("./order"));
+router.use("/item", require("./item"));
+
+//Handles all uncaught error by sending them to client
+router.use((error: Error, req, res, next) => {
+    if(env === 'development') {
+        //devs get a stack trace
+        return res.status(500).json(error.stack);
+    } else {
+        //clients get plain error message
+        return res.status(500).json(error.toString());
+    }
+})
 
 export default router;
--- src/utils.ts
+++ src/utils.ts
@@ -38,3 +38,31 @@ export function isTokeninvalid(req: Request, res: Response) {
 export function requestPermission(res: Response, permission: string) {
   return res.status(401).send("Access denied, You need permission to " + permission + " .");
 }
+
+/**
+ * check if client provided an valid id,
+ * respond if not, return true if id is valid.
+ * Does not query the id, so row may not exist.
+ */
+export function isIdValid(req:Request, res:Response) {
+    const id = req.body.id;
+    if(id === undefined || id === null) {
+        res.status(400).send("You must provide an id.");
+        return false;
+    }
+    if(parseInt(id) != req.body.id) {
+        res.status(400).send("Id must be an integer.");
+        return false;
+    }
+
+    return true;
+}
+
+/**
+ * accepts an async route handling method, wraps it and make it to pass error to next middleware
+ */
+export function catchError(cb:(req: Request, res: Response) => any) {
+  return (req, res, next) => {
+    cb(req, res).catch(next);
+  };
+}
