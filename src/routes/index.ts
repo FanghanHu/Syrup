6282047f251148b0df8f064ee48c276e95a8c4c2
Fanghan Hu <Fanghan.Hu@gmail.com>
2021-03-16 22:59:20 -0500

    add a service worker

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -8,6 +8,10 @@ const router = express.Router();
 // API Routes
 router.use("/api", apiRoutes);
 
+router.get("/service-worker.js", function(req, res) {
+    res.sendFile(path.join(__dirname, "../../client/build/service-worker.js"));
+});
+
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
     res.sendFile(path.join(__dirname, "../../client/build/index.html"));
